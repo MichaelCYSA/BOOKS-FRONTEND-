39cdb0f547d1e18ebf81089857911672d42f8e49
Misha Kysa <wertygo1997@gmail.com>
2022-01-31 12:31:33 +0200

    favorites

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,14 @@ import debounce from "lodash/debounce"
 import { useRouter } from 'next/router'
 
 import styles from '../styles/Main.module.scss'
+import NavList from '../components/navLinks'
 
 export default function Main({ search }) {
 
   const searchInput = React.useRef(null)
   const [dataBooks, setDataBooks] = React.useState(null)
+  const [favoriteItem, setFavoriteItem] = React.useState(null)
+  const [favoriteList, setFavoriteList] = React.useState({})
   const router = useRouter()
 
   // get books 
@@ -21,7 +24,7 @@ export default function Main({ search }) {
       }
     }
   }
-  
+
   const handleSearch = debounce(async ({ target: { value } = {} }) => {
     router.push(
       {
@@ -41,11 +44,35 @@ export default function Main({ search }) {
     }
     searchInput.current.addEventListener("input", handleSearch, false);
   }, [])
-  
+
+  // favorite add logic
+  React.useEffect(() => {
+    if (localStorage.getItem('favorites')) {
+      setFavoriteList(JSON.parse(localStorage.getItem('favorites')))
+    }
+    if (favoriteItem) {
+      let favoriteListObject = {}
+      if (favoriteList[favoriteItem.id]) {
+        Object.keys(favoriteList).map((favoriteEl) => {
+          if (favoriteEl === favoriteItem.id) {
+            return
+          }
+          favoriteListObject[favoriteEl] = favoriteList[favoriteEl]
+        })
+      } else {
+        favoriteListObject = { ...favoriteList, [favoriteItem.id]: favoriteItem }
+      }
+      localStorage.setItem('favorites', JSON.stringify(favoriteListObject))
+      setFavoriteItem(null)
+    }
+  }, [favoriteItem])
+
   return (
     <React.Fragment>
       <header className={styles.header}>
-        <nav className={styles.header__nav}></nav>
+        <NavList
+          search={router.query.search}
+        />
         <input
           ref={searchInput}
           className={styles.header__search}
@@ -55,25 +82,44 @@ export default function Main({ search }) {
       <main className={styles.main}>
         <section className={styles.main__wrapper}>
           {
-            dataBooks?.items?.map((book, index) => (
-              <Link key={index} href={`/book/${book.selfLink?.split('/').reverse()[0]}`}>
-                <div className={styles.main__wrapper__books}>
+            dataBooks && dataBooks.items ? dataBooks.items.map((book, index) => (
+
+              <div key={index} className={styles.main__wrapper__books}>
+                <Link href={`/book/${book.id}`}>
                   <h1 className={styles.books__title}>{book.volumeInfo?.title}</h1>
+                </Link>
+                <div className={styles['add-favorite__container']}>
                   {
-                    book.volumeInfo.imageLinks?.smallThumbnail &&
-                    <img src={book.volumeInfo.imageLinks?.smallThumbnail} className={styles.books__img} />
-                  }
-                  {
-                    book.volumeInfo.subtitle &&
-                    <p className={styles.books__subtitle}>{book.volumeInfo.subtitle}</p>
-                  }
-                  {
-                    book.searchInfo?.textSnippet &&
-                    <p className={styles['books__search-info']}>{book.searchInfo.textSnippet}</p>
+                    favoriteList[book.id] ?
+                      <img className={styles['add-favorite']} onClick={() => { setFavoriteItem(book) }} src={'/2.svg'} />
+                      :
+                      <img className={styles['add-favorite']} onClick={() => { setFavoriteItem(book) }} src={'/1.svg'} />
                   }
                 </div>
-              </Link>
+                {
+                  book.volumeInfo.imageLinks?.smallThumbnail &&
+                  <img src={book.volumeInfo.imageLinks?.smallThumbnail} className={styles.books__img} />
+                }
+                {
+                  book.volumeInfo.subtitle &&
+                  <p className={styles.books__subtitle}>{book.volumeInfo.subtitle}</p>
+                }
+                {
+                  book.searchInfo?.textSnippet &&
+                  <p className={styles['books__search-info']}>{book.searchInfo.textSnippet}</p>
+                }
+              </div>
+
             ))
+              :
+              <div className={styles.empty}>
+                <h1
+                  onClick={() => searchInput.current.focus()}
+                  className={styles['empty__search']}
+                >
+                  This page is empty click for starting search!
+                </h1>
+              </div>
           }
         </section>
       </main>
